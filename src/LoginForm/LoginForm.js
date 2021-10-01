179f194033f51d3fe4cb36bf575e7a7da9c232f2
methot-ilya-3ds <64415447+methot-ilya-3ds@users.noreply.github.com>
2021-10-01 14:59:47 -0400

    Login Form Commit

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -1,10 +1,44 @@
-import { Component } from 'react';
-import './LoginForm.css';
+import { Component, useState } from "react";
+import "./LoginForm.css";
 
-class LoginForm extends Component {
-  render() {
-    return null;
-  }
-}
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const SubmitLogin = ({ user, pass, event }) => {
+    event.preventDefault();
+    console.log(
+      `I submitted a login with username: ${user} and password: ${pass}`
+    );
+  };
+
+  return (
+    <>
+      <div className="loginWrapper">
+        <h1 className="loginTitle">LOG IN</h1>
+        <form
+          className="loginForm"
+          onSubmit={(e) =>
+            SubmitLogin({ user: username, pass: password, event: e })
+          }
+        >
+          <input
+            onInput={(e) => setUsername(e.target.value)}
+            type="text"
+            placeholder="username"
+            className="loginInput"
+          ></input>
+          <input
+            onInput={(e) => setPassword(e.target.value)}
+            type="text"
+            placeholder="password"
+            className="loginInput"
+          ></input>
+          <input className="loginSubmit" type="submit" value="Login"></input>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default LoginForm;
